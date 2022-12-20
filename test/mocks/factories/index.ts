22f56a345f780316aafbf90778f6e826be6d3e58
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-12-20 15:34:00 -0300

    feat(Test): Implements makeMockedGetBalance factory

--- test/mocks/factories/index.ts
+++ test/mocks/factories/index.ts
@@ -1,3 +1,4 @@
 export * from './makeClient';
 export * from './makeInMemoryCreateClient';
+export * from './makeMockedGetBalance';
 export * from './makeMockedLoginClient';
--- test/mocks/repositories/index.ts
+++ test/mocks/repositories/index.ts
@@ -1,1 +1,2 @@
+export * from './InMemoryAccountRepository';
 export * from './InMemoryClientRepository';
