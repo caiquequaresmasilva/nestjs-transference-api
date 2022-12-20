63d8d9afa8e59484b550168a799bdcef91befbc7
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-12-19 22:03:10 -0300

    feat(Domain): Implements Client entity

---
+++ src/domain/Client.ts
@@ -0,0 +1,32 @@
+import { Account } from './Account';
+import { BaseEntity } from './BaseEntity';
+
+export interface ClientProps {
+  username: string;
+  password: string;
+}
+
+export class Client extends BaseEntity<ClientProps> {
+  private _account: Account;
+
+  constructor(props: ClientProps, id?: string) {
+    super(props, id);
+    this._account = new Account();
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get username(): string {
+    return this.props.username;
+  }
+
+  public get password(): string {
+    return this.props.password;
+  }
+
+  public get account(): Account {
+    return this._account;
+  }
+}
