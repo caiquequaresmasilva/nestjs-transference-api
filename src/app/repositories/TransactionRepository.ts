17a8842756487ddb47d05ae2d51d2c5deef864ef
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-12-20 21:37:20 -0300

    feat(App): Implements findManyByAccountId abstract method on TransactionRepository

--- src/app/repositories/TransactionRepository.ts
+++ src/app/repositories/TransactionRepository.ts
@@ -2,4 +2,5 @@ import { Transaction } from '@domain/Transaction';
 
 export abstract class TransactionRepository {
   abstract create(data: Transaction): Promise<void>;
+  abstract findManyByAccountId(accountId: string): Promise<Transaction[]>;
 }
