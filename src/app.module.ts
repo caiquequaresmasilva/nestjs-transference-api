7db75f6a430dfdae46ca0e4ec49a66af50d8f6be
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-12-21 22:27:03 -0300

    feat(Infra): Implements nest app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
+import { DatabaseModule } from '@infra/database/database.module';
+import { HttpModule } from '@infra/http/http.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), HttpModule, DatabaseModule],
 })
 export class AppModule {}
